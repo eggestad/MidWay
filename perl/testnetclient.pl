e20b6b692af6964b985e2fd332242bd983f4c817
eggestad <terje.eggestad@gmail.com>
2000-09-24 14:03:39 +0000

    *** empty log message ***

--- perl/testnetclient.pl
+++ perl/testnetclient.pl
@@ -6,18 +6,27 @@ $rc = mwattach "srbp://localhost:11000";
 
 print "mwattach => $rc\n";
 
+print "press return to continue --";
 getc;
+
+print "Doing call to service \"test\", data \"date\"\n";
 
 @x = mwcall "test", "date";
 
 print "mwcall => (", scalar(@x), "): (", join (",", @x), ")\n";
 
+print "press return to continue --";
 getc;
 
+print "Doing call to service \"test\", data \"date\" NOREPLY\n";
 @x = mwcall "test", "date", MWNOREPLY();
 
 print "mwcall NOREPLY => (", scalar(@x), "): (", join (",", @x), ")\n";
 
+print "press return to continue --";
 getc;
 
+print "detaching...";
 mwdetach();
+print " bye.\n";
+

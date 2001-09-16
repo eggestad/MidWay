177bcbe2f918a5a60ab1a197d12b16522b98a72e
eggestad <terje.eggestad@gmail.com>
2001-09-16 00:05:56 +0000

    Wrong Licence header

--- src/lib/SRBclient.c
+++ src/lib/SRBclient.c
@@ -1,19 +1,19 @@
 /*
-  MidWay
+  The MidWay API
   Copyright (C) 2000 Terje Eggestad
 
-  MidWay is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License as
+  The MidWay API is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public License as
   published by the Free Software Foundation; either version 2 of the
   License, or (at your option) any later version.
   
-  MidWay is distributed in the hope that it will be useful,
+  The MidWay API is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  General Public License for more details.
+  Library General Public License for more details.
   
-  You should have received a copy of the GNU General Public License 
-  along with the MidWay distribution; see the file COPYING.  If not,
+  You should have received a copy of the GNU Library General Public
+  License along with the MidWay distribution; see the file COPYING. If not,
   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
   Boston, MA 02111-1307, USA. 
 */
@@ -24,6 +24,9 @@ static char * RCSName = "$Name$"; /* CVS TAG */
 
 /*
  * $Log$
+ * Revision 1.4  2001/09/16 00:05:56  eggestad
+ * Wrong Licence header
+ *
  * Revision 1.3  2000/09/21 18:21:55  eggestad
  * Major work, added acall/fetch and got it to work
  *
@@ -373,7 +376,7 @@ int _mwattach_srb(mwaddress_t *mwadr, char * name,
 	    errno);
       return -errno;
     };
-    rc = connect(s,mwadr->ipaddress_v4, 
+    rc = connect(s, (struct sockaddr *) mwadr->ipaddress_v4, 
 				 sizeof(struct sockaddr_in));
     if (rc == -1) {
       mwlog(MWLOG_ERROR, "_mwattach_srb: TCP/IP socket connect failed with errno=%d", 

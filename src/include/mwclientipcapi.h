ba847df80a253ae32077a3f1dfa37b82e589f58c
eggestad <terje.eggestad@gmail.com>
2002-02-28 21:58:34 +0000

    src/include/SRBclient.h

---
+++ src/include/mwclientipcapi.h
@@ -0,0 +1,23 @@
+/*
+  The MidWay API
+  Copyright (C) 2000 Terje Eggestad
+
+  The MidWay API is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public License as
+  published by the Free Software Foundation; either version 2 of the
+  License, or (at your option) any later version.
+  
+  The MidWay API is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
+  
+  You should have received a copy of the GNU Library General Public
+  License along with the MidWay distribution; see the file COPYING.  If not,
+  write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+  Boston, MA 02111-1307, USA. 
+*/
+
+int _mwattachipc(int type, char * name, int key);
+int _mwdetachipc(void);
+int _mwacall_ipc(char * svcname, char * data, int datalen, int flags) ;

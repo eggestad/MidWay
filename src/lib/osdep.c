a678b5e4ab64331fd29724c3cea11e924ea5531d
Terje Eggestad <terje.eggestad@gmail.com>
2019-01-10 15:56:43 -0600

    MAC OSX (Darwin) fix

--- src/lib/osdep.c
+++ src/lib/osdep.c
@@ -20,7 +20,7 @@
 
 
 /* Our primary need is to find out if a process exists. 
-   We can't use kill(pid, 0) since it only works for procsesses of same uid.
+   We can't use kill(pid, 0) since it only works for processes of same uid.
    
    THis is fairly OS dependant, on linux we depend on /proc.
    if /proc is not mounted we return -ENODEV, 0 on no proc, 1 if proc exists.
@@ -35,21 +35,67 @@
 
 #include <MidWay.h>
 
+/**
+ * 
+ * return 1 of process exists, 0 if not, and -errno if failure
+ */
 int _mw_procowner(pid_t pid, uid_t * uid) 
 { 
   struct stat pstat;
   char path[5+10];
   int rc;
 
-  sprintf (path, "/proc/%d", pid);
+  if (access("/proc", R_OK|X_OK)  == 0) {
+     sprintf (path, "/proc/%d", pid);
+     
+     rc = stat(path, &pstat);
+     
+     if (rc == 0) {
+	if (uid != NULL) *uid = pstat.st_uid;
+	return 1;
+     };
+     
+     if (errno == ENOENT) return 0;
+     return -ENODEV;
+  }
 
-  rc = stat(path, &pstat);
+  // default back to exec ps
+  char pscmd[512];
+  sprintf(pscmd, "ps -o uid -p %d", pid);
 
-  if (rc == 0) {
-    if (uid != NULL) *uid = pstat.st_uid;
-    return 1;
-  };
+  printf("**** UISNG PS to get UID of a pid");
+  // ps will either print out two lines:
+  // UID
+  // number
+  // with an exit code of 0
+  // or print one line
+  // UID
+  // with an exit code of 1 if pid don't exists
+  
+  FILE * fp = popen(pscmd, "r");
+  if (fp == NULL) return -ENODEV;
+  char *l, line[1024];
 
-  if (errno == ENOENT) return 0;
-  return -ENODEV;
+  
+  l = fgets(line, 1024, fp);
+  if (l == NULL) {
+     return -ENODEV;
+     
+     // WE should never get here
+     return -EFAULT;
+  }
+  l = fgets(line, 1024, fp);
+  rc = pclose(fp);
+  if (rc == 1 ) {
+     printf("**** pid is dead");
+     return 0;
+  }
+  if (l == NULL) return -ENODEV;
+  errno = 0;
+  int i = atoi(line);
+  if (errno != 0) return -ENODEV;
+  printf("**** uid = %d", i);
+  if (uid != NULL) *uid = i;
+  
+  return 1;
 };
